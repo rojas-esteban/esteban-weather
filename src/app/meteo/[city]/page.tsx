803e6d6ef0cfd85e29e1119477d73da2159beabf
Esteban Rojas <esteban.jnf@gmail.com>
2025-01-21 22:41:12 +0100

    test tailwind

--- src/app/meteo/[city]/page.tsx
+++ src/app/meteo/[city]/page.tsx
@@ -1,3 +1,4 @@
+import Counter from "@/components/counter";
 import Title from "@/components/Title";
 
 
@@ -19,14 +20,20 @@ export default async function City ({
 
     return (
 		<div className=" m-auto text-center border p-4 rounded bg-gray-50/30 w-2/3 md:w-1/3 hover:bg-gray-50/50 ">
-			<Title level={2}>{city}</Title>
+			<Title level={2}>{city}, {data.sys.country} </Title>
+			<div>
+				{data.weather[0].main}, {data.weather[0].description}
+			</div>
 			<div className="text-2xl font-bold">{data.main.temp} °C</div>
 			<img className="m-auto"
 				src={`https://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
 				alt={data.weather[0].main}
 			/>
-			<div>
-				{data.weather[0].main} {data.weather[0].description}
+			<p className="text-indigo-800 border-2">Vitesse du vent: {data.wind.speed} km/h</p>
+			<h2 className="font-extrabold p-4">Min et Max Today!   <Counter/> </h2>
+			<div className="flex justify-between p-4 ">
+				<p className="font-bold text-rose-700 "> max: {data.main.temp_max}°C </p>
+				<p className="font-bold text-blue-700">min: {data.main.temp_max}°C</p>
 			</div>
 		</div>
 	);
